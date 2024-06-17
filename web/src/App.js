0bc4de99d746ccd02b0e2892362b9a4205261080
Rishabh-Jain-1 <53470085+Rishabh-Jain-1@users.noreply.github.com>
2024-06-17 12:37:22 +0000

    init

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>React Ad Blocker</h1>
+      <p>Ad blocker is active!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
